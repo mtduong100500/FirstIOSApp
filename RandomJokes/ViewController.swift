3ab36b55d0e6581fb1744127b0383cb497c0a70a
mtduong <mtduong100500@gmail.com>
2021-07-13 12:19:05 +0700

    My First IOS App

---
+++ RandomJokes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomJokes
+//
+//  Created by Duong Mai on 12/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
